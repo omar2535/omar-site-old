f4ef60745b9230e1b3c783cb27d6464b9d23a963
Omar <omar2535@gmail.com>
2019-05-06 17:08:10 -0700

    removing assetPrefix

--- next.config.js
+++ next.config.js
@@ -4,5 +4,5 @@ module.exports = {
       '/': { page: '/' }
     };
   },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/omar-site' : '',
+  // assetPrefix: process.env.NODE_ENV === 'production' ? '/omar-site' : '',
 };
